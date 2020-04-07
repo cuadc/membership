955e5832ea339365bf4f01f9bfb8e356ff41b8fb
Charlie Jonas <charlie@charliejonas.co.uk>
2020-04-07 22:40:32 +0100

    Add show member page

--- app/helpers/member_helper.rb
+++ app/helpers/member_helper.rb
@@ -1,11 +1,17 @@
 module MemberHelper
-    def list_text(member)
-      name_part = "#{member.last_name}, #{member.other_names}"
-      expiry_part = if member.expiry.present?
-        member.expiry.strftime("%d/%m/%Y")
-      else
-        "LIFE"
-      end
-      return "#{name_part} (#{member.graduation_year}) - #{expiry_part}"
+  def member_name(member)
+    "#{member.last_name}, #{member.other_names}"
+  end
+
+  def member_expiry(member)
+    if member.expiry.present?
+      member.expiry.strftime("%d/%m/%Y")
+    else
+      "LIFE"
     end
   end
+
+  def list_text(member)
+    return "#{member_name(member)} (#{member.graduation_year}) - #{member_expiry(member)}"
+  end
+end
