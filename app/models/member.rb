b15f2c0740f49fac340958e34b91190b1693dec4
Charlie Jonas <charlie@charliejonas.co.uk>
2020-04-16 23:20:34 +0100

    Add member model validation

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,6 +2,16 @@ class Member < ApplicationRecord
   belongs_to :institution
   belongs_to :type
 
+  validates :primary_email, presence: true
+  validates :graduation_year, presence: true
+  validate :must_have_a_name
+
+  def must_have_a_name
+    if full_name.blank?
+      errors.add(:base, "Member must have a name")
+    end
+  end
+
   def full_name
     if other_names.nil?
       last_name
