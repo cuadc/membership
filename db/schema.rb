e0f6fadfbbd2562d1cc19d880709c8168c380f3f
Charlie Jonas <charlie@charliejonas.co.uk>
2020-04-25 20:08:38 +0100

    Use full UTF8 character encoding

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2020_04_23_221453) do
 
-  create_table "institutions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "institutions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
   end
 
-  create_table "members", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "members", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "camdram_id"
     t.string "crsid"
     t.string "last_name"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2020_04_23_221453) do
     t.index ["type_id"], name: "index_members_on_type_id"
   end
 
-  create_table "provider_accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "provider_accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "provider", null: false
     t.string "uid", null: false
     t.bigint "user_id", null: false
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2020_04_23_221453) do
     t.index ["user_id"], name: "index_provider_accounts_on_user_id"
   end
 
-  create_table "sessions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "sessions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.datetime "login_at", null: false
     t.datetime "expires_at", null: false
@@ -55,11 +55,11 @@ ActiveRecord::Schema.define(version: 2020_04_23_221453) do
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
-  create_table "types", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "types", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
