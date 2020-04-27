b11c4899d56a358f57fcaf8118f53779f5690f4e
Charlie Jonas <charlie@charliejonas.co.uk>
2020-04-27 17:48:07 +0100

    Present member expiry dates in nicer format

--- app/helpers/member_helper.rb
+++ app/helpers/member_helper.rb
@@ -10,4 +10,10 @@ module MemberHelper
   def list_text(member)
     return "#{member.full_name} (#{member.graduation_year}) - #{member_expiry(member)}"
   end
+
+  def expiry_text(member)
+    if member.expiry
+      member.expiry.strftime("Expires on %a #{member.expiry.day.ordinalize} %b %Y")
+    end
+  end
 end
