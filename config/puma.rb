a17538ff1bbe17f9b7014c474f6cb9330e8308d6
Charlie Jonas <charlie@charliejonas.co.uk>
2020-06-16 19:51:57 +0100

    Bind Puma to a UNIX socket in production

--- config/puma.rb
+++ config/puma.rb
@@ -8,31 +8,26 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
-
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
-
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
+# Specifies the `environment` that Puma will run in.
 #
-# preload_app!
+environment ENV.fetch("RAILS_ENV") { "development" }
 
-# Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+# Specifies the `port` or `socket` that Puma will listen on to receive requests.
+#
+if environment == 'production'
+  prune_bundler
+  bind      'unix:///var/run/membership/puma.sock?umask=0077'
+else
+  port      ENV.fetch("PORT") { 3000 }
+end
+
+# Specifies the `pidfile` that Puma will use.
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
