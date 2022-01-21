5ec185372148f24eebd9c0c1f1ddba8095072253
Charlie Jonas <charlie@charliejonas.co.uk>
2022-01-21 19:59:52 +0000

    Improve Git description

--- lib/membership/version.rb
+++ lib/membership/version.rb
@@ -2,11 +2,7 @@ module Membership
   module Version
     class << self
       def git_desc
-        @git_desc ||= `git describe --tags`.chomp.freeze
-      end
-
-      def git_commit
-        @git_commit ||= `git rev-parse --short HEAD`.chomp.freeze
+        @git_desc ||= `git describe --tags --always --dirty`.chomp.freeze
       end
 
       def git_date
