eb1a5ecad9b0cfc16bb9245a72c4770bd93df4c0
Charlie Jonas <charlie@charliejonas.co.uk>
2022-01-24 22:17:53 +0000

    Add basic mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,10 +61,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "membership_production"
 
   config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'noreply@membership.cuadc.org' }
+  config.action_mailer.delivery_method = :sendmail
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
