768a93f0b6747b510efdc24282f0d648e87dc6b8
Charlie Jonas <charlie@charliejonas.co.uk>
2022-01-24 22:43:44 +0000

    Redesign Members index page (again)

--- app/helpers/member_helper.rb
+++ app/helpers/member_helper.rb
@@ -7,19 +7,6 @@ module MemberHelper
     end
   end
 
-  def member_index_text(member)
-    return "#{member.name} (#{member.graduation_year}) - #{member_description(member)}"
-  end
-
-  def member_description(member)
-    return member.type.name if member.type.id == 999
-    if member.expiry.present?
-      "#{member.type.name} Member until #{member.expiry.strftime("%d/%m/%Y")}"
-    else
-      "#{member.type.name} Member for life"
-    end
-  end
-
   def expiry_text(member)
     if member.expiry
       member.expiry.strftime("Expires on %a #{member.expiry.day.ordinalize} %b %Y")
