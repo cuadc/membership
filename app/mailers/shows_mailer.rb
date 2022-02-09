2bad2f82de5ae78316c27fc35b7c10800687d5de
Charlie Jonas <charlie@charliejonas.co.uk>
2022-02-09 18:09:30 +0000

    Change show report mailer addresses

--- app/mailers/shows_mailer.rb
+++ app/mailers/shows_mailer.rb
@@ -3,6 +3,10 @@
 class ShowsMailer < ApplicationMailer
   def report_email
     @shows = Membership::Camdram.client.get_society("cambridge-university-amateur-dramatic-club").shows
-    mail(to: params.try(:fetch, :to) || User.pluck(:email), subject: 'CUADC Show Membership Report')
+    to_addr = params.try(:fetch, :to) || [
+      'president@cuadc.org',
+      'members@cuadc.org'
+    ]
+    mail(to: to_addr, bcc: 'chtj2@srcf.net', subject: 'CUADC Show Membership Report')
   end
 end
