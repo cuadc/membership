0851ee6ba0a4d7a766e2641631edfbb98d033e2e
Charlie Jonas <charlie@charliejonas.co.uk>
2022-04-11 21:26:07 +0100

    Set Reply-To header for new signup mail

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -5,7 +5,9 @@ class WelcomeMailer < ApplicationMailer
     @member = params[:member]
     @request_uuid = params[:request_uuid]
     to_addr = overridable_to('members@cuadc.org')
-    mail(to: to_addr, bcc: 'chtj2@srcf.net', subject: 'CUADC Membership Signup')
+    reply_to_addr = [@member.primary_email]
+    reply_to_addr << @member.secondary_email unless @member.secondary_email.nil?
+    mail(to: to_addr, reply_to: reply_to_addr, bcc: 'chtj2@srcf.net', subject: 'CUADC Membership Signup')
   end
 
   def thank_you_email
