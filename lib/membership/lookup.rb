d1e848f4582227b8bc6bbb9fb63ff4c7f68a771f
Charlie Jonas <charlie@charliejonas.co.uk>
2022-04-21 18:26:25 +0100

    Add appropriate headers to Lookup API requests

--- lib/membership/lookup.rb
+++ lib/membership/lookup.rb
@@ -20,6 +20,8 @@ module Membership
         uri = URI('https://chtj2.user.srcf.net/cgi-bin/lookup')
         uri.query = URI.encode_www_form(crsid: crsid)
         req = Net::HTTP::Get.new(uri)
+        req['Cache-Control'] = 'no-store, max-age=0'
+        req['User-Agent'] = "CUADC Membership System/#{Membership::Version.git_desc} (+https://github.com/cuadc/membership)"
         req.basic_auth ENV['LOOKUP_API_USERNAME'], ENV['LOOKUP_API_PASSWORD']
         res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http| http.request(req) }
         if res.is_a?(Net::HTTPSuccess)
