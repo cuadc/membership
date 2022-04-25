b1c9ee10b990b4a890cf3485ef5005a76876ad2d
Charlie Jonas <charlie@charliejonas.co.uk>
2022-04-26 00:41:50 +0100

    Filter auth tokens and recaptcha responses

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, 'WLS-Response']
+Rails.application.config.filter_parameters += [
+  :password, :authenticity_token,
+  'WLS-Response', 'g-recaptcha-response'
+]
