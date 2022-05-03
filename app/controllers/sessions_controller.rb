8b181c9d0c518bdde641ffaa8b546929939f5967
Charlie Jonas <charlie@charliejonas.co.uk>
2022-05-03 19:10:19 +0100

    Notify sysops of failed login attempts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,10 +9,31 @@ class SessionsController < ApplicationController
     reset_session
     auth_hash = request.env["omniauth.auth"]
     user = User.from_omniauth(auth_hash)
-    redirect_to login_url and return if user.nil?
-    sesh = Session.from_user_and_request(user, request)
-    session[:sesh_id] = sesh.id
-    redirect_to root_url
+    if user.nil?
+      ApplicationMailer.new.mail(
+        to: User.where(sysop: true).pluck(:email),
+        subject: 'CUADC Membership Login Attempt Denied',
+        body: <<~END
+          Hello,
+
+          This is an automated message from the CUADC Membership System. Please do
+          not reply.
+
+          An unsuccessful login attempt has just been made, the details of which
+          are included below:
+          #{auth_hash}
+
+          Kind regards,
+
+          The CUADC Membership System
+        END
+      ).deliver
+      redirect_to login_url and return
+    else
+      sesh = Session.from_user_and_request(user, request)
+      session[:sesh_id] = sesh.id
+      redirect_to root_url
+    end
   end
 
   def destroy
