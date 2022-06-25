95ac5a2b79987cee66d72c3db609fbe8559a632e
Charlie Jonas <charlie@charliejonas.co.uk>
2022-06-25 01:01:58 +0100

    Do not CC graduation email reminders

--- app/mailers/graduating_mailer.rb
+++ app/mailers/graduating_mailer.rb
@@ -6,7 +6,6 @@ class GraduatingMailer < ApplicationMailer
     raise ArgumentError, 'not an Ordinary Member' if @member.mtype_id != 1
     to_addr = [@member.primary_email]
     to_addr << @member.secondary_email unless @member.secondary_email.nil?
-    mail(to: to_addr.uniq, bcc: 'chtj2@srcf.net', from: 'bookkeeping@membership.cuadc.org',
-      subject: 'CUADC Membership After Graduation')
+    mail(to: to_addr.uniq, from: 'bookkeeping@membership.cuadc.org', subject: 'CUADC Membership After Graduation')
   end
 end
