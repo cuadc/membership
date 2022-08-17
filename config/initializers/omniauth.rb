5ebb893f7ec5a2943b6b20ac08d9cd6292cc88bb
Charlie Jonas <charlie@charliejonas.co.uk>
2022-08-17 19:19:25 +0100

    Do not force explicit re-auth at login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   key_path = Rails.root.join("config", "raven-pubkey")
   key_data = [[2, key_path]]
-  opts = { desc: "CUADC Membership System", msg: "you need to verify you are authorised to use this system", iact: "yes" }
+  opts = { desc: "CUADC Membership System", msg: "you need to verify you are authorised to use this system" }
   provider :ucamraven, key_data, opts
 end
