fef96cac430d577c5d51a2da7d384859d1600bfc
Charlie Jonas <charlie@charliejonas.co.uk>
2022-08-26 15:42:22 +0100

    Try forcible YAML->JSON PaperTrail serialization conversion

--- db/migrate/20220826140936_convert_paper_trail_yaml_to_json.rb
+++ db/migrate/20220826140936_convert_paper_trail_yaml_to_json.rb
@@ -3,9 +3,9 @@ class ConvertPaperTrailYamlToJson < ActiveRecord::Migration[6.1]
     ActiveRecord::Base.transaction do
       ActiveRecord::Base.connection.execute('LOCK TABLE versions WRITE')
       PaperTrail::Version.where.not(object: nil).find_each do |version|
-        version.update_column(:new_object, YAML.load(version.object))
+        version.update_column(:new_object, YAML.load(version.object).to_json)
         if version.object_changes
-          version.update_column(:new_object_changes, YAML.load(version.object_changes))
+          version.update_column(:new_object_changes, YAML.load(version.object_changes).to_json)
         end
       end
       ActiveRecord::Base.connection.execute('UNLOCK TABLES')
