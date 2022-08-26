43ae3543590b2deff41d379f6b04e4038fef4e05
Charlie Jonas <charlie@charliejonas.co.uk>
2022-08-26 15:25:57 +0100

    Run PaperTrail serialization migrations in single transaction with table lock

--- db/migrate/20220826140936_convert_paper_trail_yaml_to_json.rb
+++ db/migrate/20220826140936_convert_paper_trail_yaml_to_json.rb
@@ -1,10 +1,14 @@
 class ConvertPaperTrailYamlToJson < ActiveRecord::Migration[6.1]
   def up
-    PaperTrail::Version.where.not(object: nil).find_each do |version|
-      version.update_column(:new_object, YAML.load(version.object))
-      if version.object_changes
-        version.update_column(:new_object_changes, YAML.load(version.object_changes))
+    ActiveRecord::Base.transaction do
+      ActiveRecord::Base.connection.execute('LOCK TABLE versions WRITE')
+      PaperTrail::Version.where.not(object: nil).find_each do |version|
+        version.update_column(:new_object, YAML.load(version.object))
+        if version.object_changes
+          version.update_column(:new_object_changes, YAML.load(version.object_changes))
+        end
       end
+      ActiveRecord::Base.connection.execute('UNLOCK TABLES')
     end
   end
 
