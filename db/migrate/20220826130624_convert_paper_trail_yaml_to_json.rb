3c7f296c6d5e739c57f5b4385e4107ec284b54a2
Charlie Jonas <charlie@charliejonas.co.uk>
2022-08-26 14:49:16 +0100

    Fix uninitialized constant bug

--- db/migrate/20220826130624_convert_paper_trail_yaml_to_json.rb
+++ db/migrate/20220826130624_convert_paper_trail_yaml_to_json.rb
@@ -1,4 +1,8 @@
 class ConvertPaperTrailYamlToJson < ActiveRecord::Migration[6.1]
+  # The largest text column available in all supported RDBMS.
+  # See `create_versions.rb` for details.
+  TEXT_BYTES = 1_073_741_823
+
   def up
     add_column :versions, :new_object, :text, limit: TEXT_BYTES
     add_column :versions, :new_object_changes, :text, limit: TEXT_BYTES
