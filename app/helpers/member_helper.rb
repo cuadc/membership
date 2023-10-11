bef3eb888bae825e6a321204310582adcbe4a8ab
Charlie Jonas <charlie@charliejonas.co.uk>
2023-10-11 19:11:47 +0100

    Improve handling in face of adverse Lookup data

--- app/helpers/member_helper.rb
+++ app/helpers/member_helper.rb
@@ -1,9 +1,11 @@
 module MemberHelper
   def get_lookup_attribute(data, attribName)
+    return nil unless data['attributes'].present?
     data['attributes'].find { |attrib| attrib['scheme'] == attribName }.try(:dig, 'value')
   end
 
   def get_lookup_photo(data)
+    return nil unless data['attributes'].present?
     data['attributes'].find { |attrib| attrib['scheme'] == 'jpegPhoto' }.try(:dig, 'binaryData')
   end
 end
