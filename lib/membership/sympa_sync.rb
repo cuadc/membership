7f6c0594a62b207999b7e9931cb9683c6dc9cbaf
Charlie Jonas <charlie@charliejonas.co.uk>
2023-10-18 21:50:52 +0100

    Refactor SOAP attributes

--- lib/membership/sympa_sync.rb
+++ lib/membership/sympa_sync.rb
@@ -3,6 +3,7 @@ module Membership
     SERVER = 'https://lists.cam.ac.uk/sympa/wsdl'
     LIST = 'soc-adc-members'
     QUIET = true
+    ATTRIBUTES = { "xmlns:ns0" => "urn:sympasoap" }
 
     class << self
       def sync_members(enumerator)
@@ -15,10 +16,10 @@ module Membership
         # does in order for anything to work.
         client = Savon.client(wsdl: SERVER, env_namespace: 'soap-env', namespace_identifier: 'ns0')
         login_msg = { email: ENV['SYMPA_USERNAME'], password: ENV['SYMPA_PASSWORD'] }
-        response = client.call(:login, message: login_msg, attributes: { "xmlns:ns0" => "urn:sympasoap" })
+        response = client.call(:login, message: login_msg, attributes: ATTRIBUTES)
         cookies = HTTPI::Cookie.new(response.http.headers['set-cookie2'].dup)
 
-        response = client.call(:review, message: { 'list' => LIST }, cookies: cookies, attributes: { "xmlns:ns0" => "urn:sympasoap" })
+        response = client.call(:review, message: { 'list' => LIST }, cookies: cookies, attributes: ATTRIBUTES)
         subscribers = response.body[:review_response][:return][:item]
 
         for member in enumerator
@@ -26,13 +27,13 @@ module Membership
           emails.delete(member.contact_email)
           emails.each do |email|
             if subscribers.include?(email)
-              client.call(:del, message: { 'list' => LIST, 'email' => email, 'quiet' => QUIET }, cookies: cookies, attributes: { "xmlns:ns0" => "urn:sympasoap" })
+              client.call(:del, message: { 'list' => LIST, 'email' => email, 'quiet' => QUIET }, cookies: cookies, attributes: ATTRIBUTES)
             end
           end
           next if member.contact_email.blank?
           next if member.contact_email.start_with?("unknown-member-email-") && member.contact_email.end_with?("@cuadc.org")
           unless subscribers.include?(member.contact_email)
-            client.call(:add, message: { 'list' => LIST, 'email' => member.contact_email, 'gecos' => member.name, 'quiet' => QUIET }, cookies: cookies, attributes: { "xmlns:ns0" => "urn:sympasoap" })
+            client.call(:add, message: { 'list' => LIST, 'email' => member.contact_email, 'gecos' => member.name, 'quiet' => QUIET }, cookies: cookies, attributes: ATTRIBUTES)
           end
         end
         nil
